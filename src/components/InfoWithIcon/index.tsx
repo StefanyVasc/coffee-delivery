5aa887ff92dd01d1ae6598d39975b3817009b609
StefanyVasc <stefany.vasc.sa@gmail.com>
2024-01-05 00:57:38 -0300

    feat: create component for InfoWithIcons

---
+++ src/components/InfoWithIcon/index.tsx
@@ -0,0 +1,17 @@
+import { InfoWithIconContainer, IconContainer } from './styles'
+import { ReactNode } from 'react'
+
+interface BenefitItemProps {
+  icon: ReactNode
+  text: string | ReactNode
+  iconbg: string
+}
+
+export function InfoWithIcon({ icon, text, iconbg }: BenefitItemProps) {
+  return (
+    <InfoWithIconContainer>
+      <IconContainer iconbg={iconbg}>{icon}</IconContainer>
+      {typeof text === 'string' ? <p>{text}</p> : text}
+    </InfoWithIconContainer>
+  )
+}
